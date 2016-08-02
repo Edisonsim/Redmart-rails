4ad3919548e00d052c8f659ffcf5e17f16dccd04
Edison Sim <edison_sxw1@hotmail.com>
2016-08-02 14:06:22 +0800

    fix a bug at edit page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,16 @@
 module SessionsHelper
 
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
   # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
@@ -10,11 +21,15 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def logged_in?
     current_user
     !current_user.nil?
-
   end
+
   # Returns true if the user is logged in, false otherwise.
     def log_out
       session.delete(:user_id)
