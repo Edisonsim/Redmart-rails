bbe67bfe1b31fae4e9aac5839b56c8384fac2dfd
Edison Sim <edison_sxw1@hotmail.com>
2016-08-05 09:23:18 +0800

    changing the seed problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@
 #
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "examples-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
                email: email,
